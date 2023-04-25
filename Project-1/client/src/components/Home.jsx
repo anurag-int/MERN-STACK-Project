f8e80ca04fb7c698d4ab9d69fb4265889906e681
void main() <100292340+Webwizards60@users.noreply.github.com>
2023-04-25 12:08:10 +0530

    :sparkles: Added Home & About pages

---
+++ Project-1/client/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+        <div className="home">
+            <div className="home-content">
+            <h1>Hi, I'm Anurag Gupta</h1>
+            <h2>Full Stack Developer</h2>
+            <p>
+                I'm a full stack developer with experience in React, Node, Express,
+                MongoDB, and Firebase.
+            </p>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Home;
